f7e9cce4312a43476408e239b1e9d418d260d34f
Atiqur Rahman Atiq <59407612+atiq-tech@users.noreply.github.com>
2023-03-17 10:38:29 +0600

    Null_safety

---
+++ Dart_Null_Safety/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  int? numb = 20;
+  int finalnumb = numb ?? 10;
+  print(finalnumb);
+
+  // int? numb = null;
+  // print(numb! + 1);
+
+  // int? numb = 49;
+  // if (numb != null) {
+  //   print(numb + 1);
+  // }
+
+  // int? a = null;
+  // String? b = null;
+  // List? t = null;
+  //print("Hellow");
+}
